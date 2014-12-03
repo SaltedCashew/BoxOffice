05f94ac67d8036ac16f66f89f5d646f9a8579100
SaltedCashew <bpgray@utexas.edu>
2014-12-03 14:41:16 -0600

    Small changes so far

---
+++ src/assignment6/Priority.java
@@ -0,0 +1,17 @@
+package assignment6;
+
+public class Priority implements Comparable<Priority>
+{
+
+	public Priority()
+	{
+		// TODO Auto-generated constructor stub
+	}
+@Override
+public int compareTo(Priority arg0)
+{
+	// TODO Auto-generated method stub
+	return 0;
+}
+	}
+
---
+++ src/assignment6/Seat.java
@@ -0,0 +1,25 @@
+package assignment6;
+
+public class Seat
+{
+	private String row;
+	private int seatNum;
+	private enum attribute
+	{
+		HANDICAP,
+		RESERVED,
+		AVAILABLE;
+	}
+	private enum section
+	{
+		left,
+		right,
+		middle;
+	}
+
+	public Seat()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
