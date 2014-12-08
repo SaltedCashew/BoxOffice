6e404f7b296c5e4ce3a192ece22be4fc27bc48e1
SaltedCashew <bpgray@utexas.edu>
2014-12-07 18:51:32 -0600

    Changed BO to used locks

--- src/assignment6/BoxOffice.java
+++ src/assignment6/BoxOffice.java
@@ -4,6 +4,9 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class BoxOffice implements Runnable
 {
@@ -11,6 +14,8 @@ public class BoxOffice implements Runnable
 	private String name;
 	private ArrayList<Person> servedPeople;
 	private Theater show;
+	private Lock showLock;
+	private Condition OfficeBusyCondition;
 	
 	public BoxOffice()
 	{
@@ -23,6 +28,8 @@ public class BoxOffice implements Runnable
 		name = n;
 		servedPeople = new ArrayList<Person>();
 		show = t;
+		showLock = new ReentrantLock();
+		OfficeBusyCondition = showLock.newCondition();
 	}
 	
 
@@ -31,7 +38,7 @@ public class BoxOffice implements Runnable
 		Random r =  new Random();
 		int lineSize = r.nextInt(1000);
 		line = new ArrayDeque<Person>(lineSize);
-		for(int k = 0; k < 500; k++)
+		for(int k = 0; k < lineSize; k++)
 		{
 			line.add(new Person());
 		}
@@ -48,18 +55,23 @@ public class BoxOffice implements Runnable
 	private void SellTickets()
 	{
 			Seat temp;
-			while(show.hasTickets())
-			{
-				synchronized(this)
+			try{
+				showLock.lock();
+				while(show.hasTickets())
 				{
 					temp = show.sellSeat();
+					temp.markSeatReserved();
+					if(line.isEmpty()) {buildQueue();}
+					Person tempPerson = line.pop();
+					tempPerson.setTicket(temp, name);
+					servedPeople.add(tempPerson);
 				}
+			}
+			finally{
+				showLock.unlock();
+			}
 				
-				temp.markSeatReserved();
-				Person tempPerson = line.pop();
-				tempPerson.setTicket(temp, name);
-				servedPeople.add(tempPerson);
-			}	
+				
 	}
 
 }
